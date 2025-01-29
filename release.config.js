f492281e2eaf1962448fb6b096ee62c43d80eaee
granchetti1 <134518621+granchetti1@users.noreply.github.com>
2025-01-30 00:30:52 +0100

    feat: improve semantic configuration

--- release.config.js
+++ release.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      [
-        "@semantic-release/github",
-        {
-          assets: ["dist/*.js", "dist/*.css"],
-        },
-      ],
-      [
-        "@semantic-release/git",
-        {
-          assets: ["CHANGELOG.md", "package.json"],
-          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        },
-      ],
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/*.js", "dist/*.css"],
+      },
     ],
-  };
-  
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
